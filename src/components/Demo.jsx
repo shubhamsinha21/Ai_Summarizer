cc267b13d04fd3ece1e23588830b67bacddd6677
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-14 03:48:35 +0530

    demo.jsx - partial processed | react-redux-provider

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,52 @@
-import React from "react";
-import Hero from "./Hero";
+import React, { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "/assets";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  // from this function, we are fetching an API
+  const handleSubmit = async (e) => {
+    alert("submitted");
+  };
+
+  const handleChange = (e) => {
+    setArticle({ ...article, url: e.target.value });
+  };
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="w-full flex flex-col gap-2">
+        <form
+          onSubmit={handleSubmit}
+          className="relative flex justify-center items-center"
+        >
+          <img
+            value={article.url}
+            onChange={handleChange}
+            src={linkIcon}
+            alt="link-icon"
+            className=" absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            required
+            className="url_input "
+          />
+
+          <button
+            type="submit"
+            className="submit_btn focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            <p>↵</p>
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* Provider is going to service the app with something called store */}
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
